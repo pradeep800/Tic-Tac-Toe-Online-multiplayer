fa3610ea6a67324b7df84646d976696a0d7c192b
pradeep <pradeep8b0@gmail.com>
2023-02-25 18:52:49 +0530

    add  remove room after 10 min

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import { Server, Socket } from "socket.io";
 import WinCheck, { NoTurns } from "./fun/WinCheck.js";
-
+const TimeOut = 10; //timeout for socket in minute
 const app = express();
 const server = http.createServer(app);
 let couter = 0;
@@ -23,15 +23,16 @@ interface Info {
   sucStart: number;
   names: [string | undefined, string | undefined];
   wins: [number, number];
+  lastActivity: number;
 }
 
-let map = new Map<string, Info>();
+let rooms = new Map<string, Info>();
 /**
  * this is for chaging turns
- * @param RoomId
+ *
  */
 function ChangeTurns(RoomId: string) {
-  map.get(RoomId).turn = map.get(RoomId).turn === "O" ? "X" : "O";
+  rooms.get(RoomId).turn = rooms.get(RoomId).turn === "O" ? "X" : "O";
 }
 
 io.on("connection", (socket: Socket) => {
@@ -40,32 +41,33 @@ io.on("connection", (socket: Socket) => {
    */
   socket.on("create-room", (name) => {
     let roomId = getNumber();
-    map.set(roomId, {
+    rooms.set(roomId, {
       turn: "X",
       playerId: [socket.id],
       order: ["X", "O"],
       sucStart: 0,
       names: [undefined, undefined],
       wins: [0, 0],
+      lastActivity: Date.now(),
     });
     socket.join(roomId);
-    map.get(roomId).names[0] = name;
+    rooms.get(roomId).names[0] = name;
     socket.emit("room-joined", roomId);
   });
   /**
    * when somenoe joined this event get triggers
    */
   socket.on("join", (id: string, name) => {
-    if (map.get(id)?.["playerId"].length < 2) {
-      if (map.get(id)["playerId"].includes(socket.id)) {
+    if (rooms.get(id)?.["playerId"].length < 2) {
+      if (rooms.get(id)["playerId"].includes(socket.id)) {
         console.log("error");
         socket.emit("error", "Already Joined");
       } else {
-        map.get(id)["playerId"].push(socket.id);
-        map.get(id).names[1] = name;
+        rooms.get(id)["playerId"].push(socket.id);
+        rooms.get(id).names[1] = name;
         socket.emit("room-joined");
         socket.join(id);
-        io.to(id).emit("start", map.get(id));
+        io.to(id).emit("start", rooms.get(id));
       }
     } else {
       socket.emit("error", "No More Space");
@@ -75,67 +77,90 @@ io.on("connection", (socket: Socket) => {
    * when game startes then this event is triggered
    */
   socket.on("start", (roomId, name) => {
-    map.get(roomId).sucStart++;
-    if (map.get(roomId).sucStart >= 2) {
-      map.get(roomId).sucStart = 0;
-      io.to(roomId).emit("turn", map.get(roomId).turn);
+    console.log(rooms);
+    rooms.get(roomId).sucStart++;
+    if (rooms.get(roomId).sucStart >= 2) {
+      rooms.get(roomId).sucStart = 0;
+      io.to(roomId).emit("turn", rooms.get(roomId).turn);
       ChangeTurns(roomId);
     }
   });
+  /**
+   * when player played his turned it will check if it is winning codition or it will wil chance to another player
+   */
   socket.on("played", (prev, roomId) => {
+    rooms.get(roomId).lastActivity = Date.now();
     let Xwon = WinCheck(prev, "X");
     let Owon = WinCheck(prev, "O");
     let isDraw = NoTurns(prev);
-    let order = map.get(roomId).order;
+    let order = rooms.get(roomId).order;
 
     if (Xwon) {
       let index = order[0] === "X" ? 0 : 1;
-      map.get(roomId).wins[index]++;
+      rooms.get(roomId).wins[index]++;
       io.to(roomId).emit("play", prev);
       io.to(roomId).emit(
         "win",
-        map.get(roomId).wins,
-        map.get(roomId).names,
-        map.get(roomId).names[index]
+        rooms.get(roomId).wins,
+        rooms.get(roomId).names,
+        rooms.get(roomId).names[index]
       );
     } else if (Owon) {
       let index = order[0] === "O" ? 0 : 1;
-      map.get(roomId).wins[index]++;
+      rooms.get(roomId).wins[index]++;
       io.to(roomId).emit("play", prev);
 
       io.to(roomId).emit(
         "win",
-        map.get(roomId).wins,
-        map.get(roomId).names,
-        map.get(roomId).names[index]
+        rooms.get(roomId).wins,
+        rooms.get(roomId).names,
+        rooms.get(roomId).names[index]
       );
     } else if (isDraw) {
-      io.to(roomId).emit("win", "draw", map.get(roomId).wins, "___DRAW___");
+      io.to(roomId).emit("win", "draw", rooms.get(roomId).wins, "___DRAW___");
       io.to(roomId).emit("play", prev);
     } else {
       io.to(roomId).emit("play", prev);
-      io.to(roomId).emit("turn", map.get(roomId).turn);
+      io.to(roomId).emit("turn", rooms.get(roomId).turn);
       ChangeTurns(roomId);
     }
   });
+  /**
+   * When we Want  to close this socket
+   * it will remove it from memory and close from all device which are connected to this room
+   */
   socket.on("close", (roomId) => {
-    map.delete(roomId);
+    rooms.delete(roomId);
     io.to(roomId).emit("close");
     io.of("/").adapter.rooms.delete(roomId);
   });
+  /**
+   * when you have to reset your board  and start again
+   */
   socket.on("another-match", (roomId) => {
-    map.get(roomId).order = [
-      map.get(roomId).order[1],
-      map.get(roomId).order[0],
+    rooms.get(roomId).order = [
+      rooms.get(roomId).order[1],
+      rooms.get(roomId).order[0],
     ];
-    map.get(roomId).turn = "X";
+    rooms.get(roomId).turn = "X";
     io.to(roomId).emit("another-match");
     io.to(roomId).emit("play", ["", "", "", "", "", "", "", "", ""]);
-    io.to(roomId).emit("start", map.get(roomId));
+    io.to(roomId).emit("start", rooms.get(roomId));
   });
 });
-// ["", "", "", "", "", "", "", "", ""]
-
+/**
+ * It every 1 minnute if there is some room with are in active for Timout time
+ */
+setInterval(() => {
+  let ThisPoint = Date.now();
+  for (let [roomId, room] of rooms) {
+    if (ThisPoint - room.lastActivity > TimeOut * 60 * 1000) {
+      rooms.delete(roomId);
+      io.to(roomId).emit("close");
+      io.of("/").adapter.rooms.delete(roomId);
+    }
+  }
+}, 60 * 1000);
 const PORT = 8000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
